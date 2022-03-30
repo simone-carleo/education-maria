aaeed3e87e67675789d50a07ec1a6c8e49274767
Ma.Oliva <ma.oliva@almaviva.it>
2022-03-30 18:06:24 +0200

    aggiunta annotazioni, application.yml e compattato codice

---
+++ src/main/java/it/adt/comparison/service/ComparisonService.java
@@ -0,0 +1,5 @@
+package it.adt.comparison.service;
+
+public interface ComparisonService {
+    void compareString(String nameCost, String name);
+}
---
+++ src/main/java/it/adt/comparison/service/impl/ComparisonServiceImpl.java
@@ -0,0 +1,24 @@
+package it.adt.comparison.service.impl;
+
+import it.adt.comparison.service.ComparisonService;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class ComparisonServiceImpl implements ComparisonService {
+
+    final static Logger logger = LogManager.getLogger(ComparisonServiceImpl.class);
+    @Override
+    public void compareString(String nameCost, String name)  {
+
+        if (StringUtils.equals(nameCost, name)) {
+            logger.info("Le stringhe nameCost {} e name {} sono uguali", nameCost, name);
+        } else {
+            logger.warn("Le stringhe nameCost {} e name {} sono differenti", nameCost, name);
+        }
+
+    }
+}
