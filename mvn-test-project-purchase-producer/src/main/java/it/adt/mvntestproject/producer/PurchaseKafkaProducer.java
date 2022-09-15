633b67e8370e0e5a9e99c10797541c159511c125
Ma.Oliva <ma.oliva@almaviva.it>
2022-09-15 11:30:02 +0200

    rimosse classi relative a kafka

--- mvn-test-project-purchase-producer/src/main/java/it/adt/mvntestproject/producer/PurchaseKafkaProducer.java
+++
@@ -1,8 +0,0 @@
-package it.adt.mvntestproject.producer;
-
-public interface PurchaseKafkaProducer {
-
-//    void sendMessage(String topic, String key, String message);
-
-//      <T> void sendMessage(String topic,String key, T message);
-}
